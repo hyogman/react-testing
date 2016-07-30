a28e421b9431a652156cc9d2b598bd6c54bdebf1
henry <henryyogman@gmail.com>
2016-07-29 18:12:53 -0600

    worked on testing commentbox, commentlist, and starting on action creator

---
+++ test/components/comment_box_test.js
@@ -0,0 +1,40 @@
+import {renderComponent, expect} from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox', () => {
+  let component;
+
+  beforeEach(() => {
+   component = renderComponent(CommentBox);
+  });
+
+  it('has the class comment-box', () => {
+    expect(component).to.have.class('comment-box');
+  });
+
+  it('has a text area', () => {
+    expect(component.find('textarea')).to.exist;
+  });
+
+  it('has a button', () => {
+    expect(component.find('button')).to.exist;
+  });
+
+  describe('entering in some text', () => {
+
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows text in text area', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('When submitted, it clears the input', () => {
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
+    });
+
+  });
+
+});
