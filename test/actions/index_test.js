8cee94ee9bd1021b5f234382c7d3a9a5e34afdd4
henry <henryyogman@gmail.com>
2016-08-01 10:57:12 -0600

    rewrote test_helper

--- test/actions/index_test.js
+++ test/actions/index_test.js
@@ -3,11 +3,14 @@ import { SAVE_COMMENT } from '../../src/actions/types';
 import { saveComment } from '../../src/actions/index';
 
 describe('actions', () => {
+
   describe('save comment', () => {
+
     it('it has the correct type', () => {
       const action = saveComment();
       expect(action.type).to.equal(SAVE_COMMENT);
     });
+
     it('it has the correct payload', () => {
       const action = saveComment('new comment');
       expect(action.payload).to.equal('new comment');
